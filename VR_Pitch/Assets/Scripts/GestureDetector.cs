3ea0afb9697664ddbc35fadedc1c96491a079a2c
Ali Shafiei <alireza.021@hotmail.com>
2020-07-23 18:15:56 -0700

    GestureDetector created

---
+++ VR_Pitch/Assets/Scripts/GestureDetector.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[System.Serializable]
+public struct Gesture
+{
+    public string name;
+    public List<Vector3> fingerDatas;
+    public UnityEvent onRecognized;
+
+}
+public class GestureDetector : MonoBehaviour
+{
+    public float threshold = 0.1f;
+    public OVRSkeleton skeleton;
+    public List<Gesture> gestures;
+    private List<OVRBone> fingerBones;
+    public bool debugMode = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (debugMode && Input.GetKeyDown(KeyCode.Space))
+        {
+            fingerBones = new List<OVRBone>(skeleton.Bones);
+            Save();
+        }
+    }
+
+    void Save() {
+        Gesture g = new Gesture();
+        g.name = "New Gesture";
+        List<Vector3> data = new List<Vector3>();
+        foreach (var bone in fingerBones)
+        {
+            data.Add(skeleton.transform.InverseTransformPoint(bone.Transform.position));
+        }
+        g.fingerDatas = data;
+        gestures.Add(g);
+    }
+
+    Gesture Recognize()
+    {
+        Gesture currentGesture = new Gesture();
+        float currentMin = Mathf.Infinity;
+
+        foreach (var gesture in gestures)
+        {
+            float sumDistance = 0;
+            bool isDiscarded = false;
+            for (int i = 0; i < fingerBones.Count; i++)
+            {
+                Vector3 currentData = skeleton.transform.InverseTransformPoint(fingerBones[i].Transform.position);
+                float distance = Vector3.Distance(currentData, gesture.fingerDatas[i]);
+                if (distance > threshold)
+                {
+                    isDiscarded = true;
+                    break;
+                }
+                sumDistance += distance;
+            }
+            if (!isDiscarded && sumDistance < currentMin)
+            {
+                currentMin = sumDistance;
+                currentGesture = gesture;
+            }
+        }
+        return currentGesture;
+
+    }
+
+
+
+
+}
