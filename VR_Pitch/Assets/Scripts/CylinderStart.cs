5533b1a8b19daa208db16ab37ee9bf530fdbf056
Ali Shafiei <alireza.021@hotmail.com>
2020-07-25 14:19:03 -0700

    Start ball added and working

---
+++ VR_Pitch/Assets/Scripts/CylinderStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderStart : MonoBehaviour
+{
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag =="StartBall") {
+            Debug.Log("StartGame");
+        }
+    }
+}
