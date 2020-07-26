78f746fc22845b391e8ed0d443e4bf0d7caffa74
Ali Shafiei <alireza.021@hotmail.com>
2020-07-25 18:13:29 -0700

    LobbyScene logic complete

---
+++ VR_Pitch/Assets/Scripts/LobbyStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyStart : MonoBehaviour
+{
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag =="StartBall") {
+            SceneManager.LoadScene(1);
+            Debug.Log("start"); 
+        }
+    }
+}
